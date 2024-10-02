0faee11e1a29cc112d0926c92e74d060dd595db5
WilliamMollet <williammdev@gmail.com>
2024-10-02 12:20:46 +0200

    synchro

--- import_data.py
+++ import_data.py
@@ -20,13 +20,17 @@ def extract_data(file_path:str,_types:list[type]):
                     values[i] = values[i].replace('Z','')
                     values[i] = values[i].split('T')
                     values[i][0] = values[i][0].split('-')
-                    y,m,d = values[i][0]
+                    y,d,m = values[i][0]
                     values[i][1] = values[i][1].split(':')
                     h,m,s = values[i][1]
+                    with open('log.txt','w') as f:
+                        f.write(f'{y} {m} {d} {h} {m} {s}\n')
                     values[i] = datetime(int(y),int(m),int(d),int(h),int(m),int(s))
+                    print(values[i])
                 else:
                     values[i] = _types[i](values[i])
-            except:
+            except Exception as e:
+                print(e)
                 values[i] = None
         list_values.append(tuple(values))
     
@@ -43,10 +47,11 @@ DATABASE = os.getenv('DATABASE')
 instance = AlwaysDataInterface(HOST, USER, PASSWORD, DATABASE)
 #table_test = instance.get_table("test")
 #print(table_test)
-#args, values = extract_data("./airport_data/planes.txt",[str,int,str,str,str,int,int,str,str])
-#instance.insert_values('planes')
-#print(args)
-#print(values)
+args, values = extract_data("./airport_data/flights.txt",[int,int,int,int,int,int,int,int,int,str,int,str,str,str,int,int,int,int,datetime])
+# year,month,day,dep_time,sched_dep_time,dep_delay,arr_time,sched_arr_time,arr_delay,carrier,flight,tailnum,origin,dest,air_time,distance,hour,minute,time_hour
+# instance.insert_values('Airline',args, values)
+# print(args)
+# print(values)
 #instance.insert_values('test',args, values)
 #print(instance.custom_command('SELECT * FROM Airport WHERE'))
 instance.close_connection()
